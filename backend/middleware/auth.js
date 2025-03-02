bbd27f372d96be3ef522dd7d0b909221cf702fc7
zionawit <tsionget22@gmail.com>
2025-03-01 20:53:16 -0800

    gitignore

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,18 +1,22 @@
-import jwt from "jsonwebtoken"
+ import jwt from 'jsonwebtoken';
 
-const authMiddleware = async (req,res) =>{
-    const {token} = req.headers;
-    if (!token){
-        return res.json({success:false,message:"Not Authorized login again"})
-    }
-    try {
-        const token_decode = jwt.verify(token,process.env.JWT_SECRET);
-        req.body.userId = token_decode.id;
-        next();
-    } catch (error) {
-        console.log(error);
-        res.json({success:false,message:"Error"})
-    }
-}
+ const authMiddleware = async (req, res, next) => {
+   // Add 'next' here
+   const { token } = req.headers;
+   if (!token) {
+     return res.json({
+       success: false,
+       message: 'Not Authorized, login again',
+     });
+   }
+   try {
+     const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+     req.body.userId = token_decode.id;
+     next(); // Call next() to proceed
+   } catch (error) {
+     console.log(error);
+     res.json({ success: false, message: 'Error' });
+   }
+ };
 
-export default authMiddleware;
+ export default authMiddleware;
